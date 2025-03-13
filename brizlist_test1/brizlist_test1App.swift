b7cbcd66acd543b343c5a4f40df4c0b5612dde06
Stephen Dawes <stephenrdawes@gmail.com>
2025-03-13 19:24:03 +0000

    Initial Commit

---
+++ brizlist_test1/brizlist_test1App.swift
@@ -0,0 +1,17 @@
+//
+//  brizlist_test1App.swift
+//  brizlist_test1
+//
+//  Created by Stephen Dawes on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct brizlist_test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
